3034efecf6cdba99a3af9901c2308d3e396276cf
Allen.Jiang <jiangjun1102@hotmail.com>
2019-06-17 09:29:55 +0800

    '更新chapter3'

--- chapter3/code.py
+++ chapter3/code.py
@@ -6,10 +6,17 @@ Created on Sat Oct 14 17:03:39 2017
 """
 
 import os
-import numpy as np
+#import numpy as np
 
 import matplotlib.pyplot as plt
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
 import pandas as pd
+
+os.chdir(os.getcwd())
+if not os.path.exists('./img'):
+    os.mkdir('./img')
+
 """
 代码说明：
 programmer_1: 制作箱线图
@@ -38,7 +45,8 @@ def programmer_1(file_name):
     # 画箱线图
     p = data.boxplot(return_type='dict')
     x = p['fliers'][0].get_xdata()
-    y = p['fliers'][0].get_ydata().sort()
+    y = p['fliers'][0].get_ydata()
+    y.sort()    # 直接修改原列表,无返回值
 
     for i in range(len(x)):
         # 处理临界情况， i=0时
@@ -46,7 +54,8 @@ def programmer_1(file_name):
         # 添加注释, xy指定标注数据，xytext指定标注的位置（所以需要特殊处理）
         plt.annotate(
             y[i], xy=(x[i], y[i]), xytext=(x[i] + 0.05 - 0.8 / temp, y[i]))
-    plt.show()
+    plt.title('箱型图')
+    plt.savefig('img/箱型图.png',dpi=200)
 
 
 def programmer_2(file_name):
@@ -79,23 +88,25 @@ def programmer_3(file_name):
         xy=(6, p[6]),
         xytext=(6 * 0.9, p[6] * 0.9),
         arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+    plt.xlim([-0.5,len(data)-0.5])
     plt.ylabel(u'盈利（比例）')
-    plt.show()
+    plt.title('盈利图(值和比例)')
+    plt.savefig('img/盈利图(值和比例).png',dpi=200)
 
 
 def programmer_4(file_name):
     catering_sale = file_name
     data = pd.read_excel(catering_sale, index_col=u'日期')
 
-    data.corr()
-    data.corr()[u'百合酱蒸凤爪']
-    data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺'])
+    print(data.corr())
+    print(data.corr()[u'百合酱蒸凤爪'])
+    print(data[u'百合酱蒸凤爪'].corr(data[u'翡翠蒸香茜饺']))
 
 
 if __name__ == "__main__":
-    path = os.getcwd()
-    # programmer_1(path + '/data/catering_sale.xls')
-    # programmer_2(path + '/data/catering_sale.xls')
-    # programmer_3(path + '/data/catering_dish_profit.xls')
-    # programmer_4(path + '/data/catering_sale_all.xls')
+#    path = os.getcwd()
+    programmer_1('data/catering_sale.xls')
+    programmer_2('data/catering_sale.xls')
+    programmer_3('data/catering_dish_profit.xls')
+    programmer_4('data/catering_sale_all.xls')
     pass
